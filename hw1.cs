ad8f483df8235108142d7131347cd6bcd9cf62eb
willp15 <156857218+willp15@users.noreply.github.com>
2024-01-18 16:12:17 -0600

    upload hw1

---
+++ hw1.cs
@@ -0,0 +1,11 @@
+using System
+
+class Program {
+    static void Main() {
+        double x = 2.5;
+        double y = 3.3;
+
+        double z = 4 * x * x + 3 * y
+        Console.WriteLine(z);
+     }
+}
